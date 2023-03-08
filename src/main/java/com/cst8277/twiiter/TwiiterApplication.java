bc40fd43883ac07ec34604f7b8c0588a23db7a3b
victor <victor@Ubuntu.myguest.virtualbox.org>
2023-03-08 08:55:21 -0500

    Initial commit

---
+++ src/main/java/com/cst8277/twiiter/TwiiterApplication.java
@@ -0,0 +1,13 @@
+package com.cst8277.twiiter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwiiterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwiiterApplication.class, args);
+	}
+
+}
