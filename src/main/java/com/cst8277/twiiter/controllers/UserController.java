3edc28e9a0be8d0a1d5c4ea8e0c7c60b93a7c088
victor <hoyhoy100@hotmail.com>
2023-04-04 16:45:01 -0400

    Initial commit

--- src/main/java/com/cst8277/twiiter/controllers/UserController.java
+++ src/main/java/com/cst8277/twiiter/controllers/UserController.java
@@ -2,13 +2,19 @@ package com.cst8277.twiiter.controllers;
 
 import com.cst8277.twiiter.Service.UserManagementService;
 import com.cst8277.twiiter.dto.Greeting;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.OAuth2AccessToken;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.*;
 
 import java.sql.SQLException;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
@@ -27,9 +33,8 @@ public class UserController {
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(null);
         }
         return ResponseEntity.status(HttpStatus.OK).body(new Greeting(counter.incrementAndGet(), String.format(template, name)));
-
-
     }
+
     @GetMapping(path = "/users")
     public ResponseEntity<Map<String, Object>> User(@RequestParam("name") String name, @RequestParam("uuid") String uuid) throws SQLException {
 
@@ -62,5 +67,10 @@ public class UserController {
         HashMap<String, Object> map = usmg.getMsg(name,uuid,username);
         return ResponseEntity.status(uuid != null ? HttpStatus.OK : HttpStatus.UNAUTHORIZED).body(map);
 
+    }
+
+    @GetMapping("/user")
+    public Map<String, Object> user(@AuthenticationPrincipal OAuth2User principal) {
+        return Collections.singletonMap("name", principal.getAttribute("name"));
     }
 }
